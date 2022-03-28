d27920aedda4dd24f59502f03b139a46adfa6399
Ivan <64332115+evanesenze@users.noreply.github.com>
2022-03-28 21:34:12 +0500

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
+import {ReactComponent as Logo} from './assets/logo.svg';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function App() {
+  return (<React.Fragment>
+      <div className='main'>
+        <nav className='main__nav'>
+            <div className='nav_logo'>
+                <a><Logo /></a>
+            </div>
+        </nav>
+        <div className='content'>
+        </div>   
+      </div>
+      <div className='now_playing_bar' ></div>
+  </React.Fragment>
+  );
+}
 
-// export default App;
+export default App;
