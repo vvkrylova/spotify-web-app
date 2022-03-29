fa5df0fc3a021091a7f5cd0ab7dae698f8ab6133
Ivan <64332115+evanesenze@users.noreply.github.com>
2022-03-29 23:42:13 +0500

    add navigation buttons

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import {ReactComponent as Search } from './assets/search.svg';
 import {ReactComponent as Collection } from './assets/collection.svg';
 // import {ReactComponent as CollectionActive } from './assets/collectionActive.svg';
 import {ReactComponent as Avatar} from './assets/avatar.svg';
+import {ReactComponent as TriangleDown} from './assets/triangleDown.svg';
+import {ReactComponent as Back} from './assets/back.svg';
+import {ReactComponent as Forward} from './assets/forward.svg';
 function App() {
   return (<React.Fragment>
       <div className='main'>
@@ -15,16 +18,16 @@ function App() {
             <div className='nav_logo'>
                 <a href="/"><Logo /></a>
             </div>
-            <ul className='nav_buttons'>
-              <li className='nav_buttons__btn nav_buttons__btn_active'>
+            <ul className='nav_btns'>
+              <li className='nav_btns__btn nav_btns__btn_active'>
                   <HomeActive />
                   <span>Главная</span>
               </li>
-              <li className='nav_buttons__btn'>
+              <li className='nav_btns__btn'>
                 <Search />
                 <span >Поиск</span>
               </li>
-              <li className='nav_buttons__btn'>
+              <li className='nav_btns__btn'>
                 <Collection />
                 <span>Моя медиатека</span>
               </li>
@@ -33,11 +36,16 @@ function App() {
         <div className='main__container'>
           <div className='top_bar'>
             <header className='top_bar__header'>
+              <div className='navigation_btns'>
+                <div className='navigation_btn navigation_btn_disabled'><Back /></div>
+                <div className='navigation_btn'><Forward /></div>
+              </div>
               <div className='profile_btn'>
                 <div className='profile_btn__avatar'>
                   <Avatar />
                 </div>
                 <span>Пользователь</span>
+                <TriangleDown />
               </div>
             </header>
           </div>
