12df1ea106b1a56cc233b53c54638db1b1bd8b64
Ivan <64332115+evanesenze@users.noreply.github.com>
2022-03-30 20:58:21 +0500

    add content

--- src/App.tsx
+++ src/App.tsx
@@ -16,43 +16,119 @@ function App() {
       <div className='main'>
         <nav className='main__nav'>
             <div className='nav_logo'>
-                <a href="/"><Logo /></a>
+                <a className='nav_logo__link' href="/"><Logo className='nav_logo__icon' /></a>
             </div>
             <ul className='nav_btns'>
               <li className='nav_btns__btn nav_btns__btn_active'>
-                  <HomeActive />
-                  <span>Главная</span>
+                  <HomeActive className='nav_btns__icon' />
+                  <span className='text_ellipsis'>Главная</span>
               </li>
               <li className='nav_btns__btn'>
-                <Search />
-                <span >Поиск</span>
+                <Search className='nav_btns__icon' />
+                <span className='text_ellipsis'>Поиск</span>
               </li>
               <li className='nav_btns__btn'>
-                <Collection />
-                <span>Моя медиатека</span>
+                <Collection className='nav_btns__icon' />
+                <span className='text_ellipsis'>Моя медиатека</span>
               </li>
+              <hr className="nav_btns__line" />
             </ul>
         </nav>
         <div className='main__container'>
           <div className='top_bar'>
             <header className='top_bar__header'>
               <div className='navigation_btns'>
-                <div className='navigation_btn navigation_btn_disabled'><Back /></div>
+                <div className='navigation_btn navigation_btn__disabled'><Back /></div>
                 <div className='navigation_btn'><Forward /></div>
               </div>
               <div className='profile_btn'>
                 <div className='profile_btn__avatar'>
                   <Avatar />
                 </div>
-                <span>Пользователь</span>
-                <TriangleDown />
+                <span className="profile_btn__titile text_ellipsis">Пользователь</span>
+                <TriangleDown className='profile_btn__show_more'/>
               </div>
             </header>
           </div>
-          <div className='content'></div>
+          <div className='content'>
+            <section className='content__section' >
+              <div className='section__title'>
+                <h2 className='section__title_text text_ellipsis'>Послушай эти шоу</h2>
+                <span className='section__show_all'>Все</span>
+              </div>
+              <div className='section__content'>
+                <div className='album_card'>
+                  <div className="album_card__image">
+                    <img className='album_card__image_content' src='https://i.scdn.co/image/09a2ab8c15ea83c46922060ca497453b5697d548'/>
+                  </div>
+                  <div className="album_card__info">
+                    <span className="album_title text_ellipsis">Шоу Talk на ТНТ.</span>
+                    <span className="album_author text_ellipsis">Телеканал ТНТ</span>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className='content__section' >
+              <div className='section__title'>
+                <h2 className='section__title_text text_ellipsis'>Выбор редакции (чарты)</h2>
+                <span className='section__show_all'>Все</span>
+              </div>
+              <div className='section__content'>
+                <div className='album_card'>
+                  <div className="album_card__image">
+                    <img className='album_card__image_content' src='https://i.scdn.co/image/09a2ab8c15ea83c46922060ca497453b5697d548'/>
+                  </div>
+                  <div className="album_card__info">
+                    <span className="album_title">Шоу Talk на ТНТ.</span>
+                    <span className="album_author">Телеканал ТНТ</span>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className='content__section' >
+              <div className='section__title'>
+                <h2 className='section__title_text text_ellipsis'>Что-то новенькое</h2>
+                <span className='section__show_all'>Все</span>
+              </div>
+              <div className='section__content'>
+                <div className='album_card'>
+                  <div className="album_card__image">
+                    <img className='album_card__image_content' src='https://i.scdn.co/image/09a2ab8c15ea83c46922060ca497453b5697d548'/>
+                  </div>
+                  <div className="album_card__info">
+                    <span className="album_title">Шоу Talk на ТНТ.</span>
+                    <span className="album_author">Телеканал ТНТ</span>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className='content__section' >
+              <div className='section__title'>
+                <h2 className='section__title_text text_ellipsis'>В тренде</h2>
+                <span className='section__show_all'>Все</span>
+              </div>
+              <div className='section__content'>
+                <div className='album_card'>
+                  <div className="album_card__image">
+                    <img className='album_card__image_content' src='https://i.scdn.co/image/09a2ab8c15ea83c46922060ca497453b5697d548'/>
+                  </div>
+                  <div className="album_card__info">
+                    <span className="album_title">Шоу Talk на ТНТ.</span>
+                    <span className="album_author">Телеканал ТНТ</span>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>   
       </div>
-      <div className='now_playing_bar' ></div>
+      <div className='now_playing_bar'>
+        <footer className="now_playing_bar__footer">
+          <div className="player__wrapper">
+            <audio className="player" controls />
+          </div>
+        </footer>
+      </div>
   </React.Fragment>
   );
 }
